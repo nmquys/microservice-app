1c6b67110c982708617850fe35de831a98018849
nmquys <mquy2810@gmail.com>
2026-04-16 21:15:19 +0700

    rebase

--- product-service/src/test/java/com/nmquys/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/nmquys/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,50 @@
 package com.nmquys.productservice;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.mongodb.MongoDBContainer;
+import org.testcontainers.shaded.org.hamcrest.Matchers;
+import static org.hamcrest.Matchers.notNullValue;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.2.6");
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup(){
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
     }
 
+    static{
+        mongoDBContainer.start();
+    }
+
+    @Test
+    void shouldCreateProduct() {
+        String requestBody = """
+                {
+                    "name": "ip16",
+                    "description": "ip16xxx",
+                    "price": 1200
+                }
+                """;
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/v1/products")
+                .then()
+                .statusCode(201);
+    }
 }
