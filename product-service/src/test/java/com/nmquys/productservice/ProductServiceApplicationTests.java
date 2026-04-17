01abef4e503b4cbc3e9cb4227799044febc94333
nmquys <mquy2810@gmail.com>
2026-04-17 19:53:09 +0700

    fix: config mongodb connection

--- product-service/src/test/java/com/nmquys/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/nmquys/productservice/ProductServiceApplicationTests.java
@@ -1,19 +1,25 @@
 package com.nmquys.productservice;
 
 import io.restassured.RestAssured;
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.mongodb.MongoDBContainer;
 import org.testcontainers.shaded.org.hamcrest.Matchers;
 import static org.hamcrest.Matchers.notNullValue;
 
+@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
     @ServiceConnection
+    @Container
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.2.6");
 
     @LocalServerPort
@@ -25,9 +31,9 @@ class ProductServiceApplicationTests {
         RestAssured.port = port;
     }
 
-    static{
-        mongoDBContainer.start();
-    }
+//    static{
+//        mongoDBContainer.start();
+//    }
 
     @Test
     void shouldCreateProduct() {
