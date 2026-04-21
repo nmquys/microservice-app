a745487e83652be0cec7219b0531fa16a8be7f8c
nmquys <mquy2810@gmail.com>
2026-04-21 19:47:16 +0700

    feat: init inventory-service

---
+++ inventory-service/src/test/java/com/nmquys/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nmquys.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class InventoryServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
